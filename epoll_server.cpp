68e080c0f112c14b606762a8e20fd6dad43982a2
chenlicong <chenlicong@123.com>
2018-07-11 18:21:47 +0800

    add select&poll&epoll test

--- epoll_server.cpp
+++ epoll_server.cpp
@@ -1,136 +1,185 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <errno.h>
+
+#include <netinet/in.h>
 #include <sys/socket.h>
-#include <sys/epoll.h>
 #include <arpa/inet.h>
-#include <fcntl.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <sys/types.h>
 
-#define MAX_EVENT_NUM           1024
-#define BUFFER_SIZE             10
-#define true                    1
-#define false                   0
+#define IPADDRESS   "127.0.0.1"
+#define PORT        8787
+#define MAXSIZE     1024
+#define LISTENQ     5
+#define FDSIZE      1000
+#define EPOLLEVENTS 100
 
-int setnonblocking(int fd)
+//函数声明
+//创建套接字并进行绑定
+static int socket_bind(const char* ip,int port);
+//IO多路复用epoll
+static void do_epoll(int listenfd);
+//事件处理函数
+static void handle_events(int epollfd,struct epoll_event *events,int num,int listenfd,char *buf);
+//处理接收到的连接
+static void handle_accpet(int epollfd,int listenfd);
+//读处理
+static void do_read(int epollfd,int fd,char *buf);
+//写处理
+static void do_write(int epollfd,int fd,char *buf);
+//添加事件
+static void add_event(int epollfd,int fd,int state);
+//修改事件
+static void modify_event(int epollfd,int fd,int state);
+//删除事件
+static void delete_event(int epollfd,int fd,int state);
+
+int main(int argc,char *argv[])
 {
-    int old_opt = fcntl(fd, F_GETFD);
-    int new_opt = old_opt | O_NONBLOCK;
-    fcntl(fd, F_SETFD, new_opt);
+    int  listenfd;
+    listenfd = socket_bind(IPADDRESS,PORT);
+    listen(listenfd,LISTENQ);
+    do_epoll(listenfd);
+    return 0;
+}
 
-    return old_opt;
-}//将文件描述符设置为非阻塞的
-
-void addfd(int epollfd, int fd, int enable_et)
+static int socket_bind(const char* ip,int port)
 {
-    struct epoll_event event;
-    event.data.fd = fd;
-    event.events = EPOLLIN;
-    if(enable_et){
-            event.events |= EPOLLET;
+    int  listenfd;
+    struct sockaddr_in servaddr;
+    listenfd = socket(AF_INET,SOCK_STREAM,0);
+    if (listenfd == -1)
+    {
+        perror("socket error:");
+        exit(1);
     }
-    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
-//    setnonblocking(fd);
-}//将文件描述符fd的EPOLLIN注册到epollfd指示的epoll内核事件表中，enable_et表示是否对fd启用ET模式
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    inet_pton(AF_INET,ip,&servaddr.sin_addr);
+    servaddr.sin_port = htons(port);
+    if (bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) == -1)
+    {
+        perror("bind error: ");
+        exit(1);
+    }
+    return listenfd;
+}
 
-void lt(struct epoll_event *events, int num, int epollfd, int listenfd)
+static void do_epoll(int listenfd)
 {
-    char buf[BUFFER_SIZE];
-    for(int i = 0; i < num; i++){
-            int sockfd = events[i].data.fd;
-            if(sockfd == listenfd){
-                    struct sockaddr_in clientaddr;
-                    socklen_t clilen = sizeof(clientaddr);
-                    int connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clilen);
-                    addfd(epollfd, connfd, false);//对connfd使用默认的lt模式
-            }else if(events[i].events & EPOLLIN){//只要socket读缓存中还有未读的数据，这段代码就会触发
-                    printf("event trigger once\n");
-                    memset(buf, '\0', BUFFER_SIZE);
-                    int ret = recv(sockfd, buf, BUFFER_SIZE-1, 0);
-                    if(ret <= 0){
-                            close(sockfd);
-                            continue;
-                    }
-                    printf("get %d bytes of content:%s\n", ret, buf);
-            }else{
-                    printf("something else happened\n");
-            }
+    int epollfd;
+    struct epoll_event events[EPOLLEVENTS];
+    int ret;
+    char buf[MAXSIZE];
+    memset(buf,0,MAXSIZE);
+    //创建一个描述符
+    epollfd = epoll_create(FDSIZE);
+    //添加监听描述符事件
+    add_event(epollfd,listenfd,EPOLLIN);
+    for ( ; ; )
+    {
+        //获取已经准备好的描述符事件
+        ret = epoll_wait(epollfd,events,EPOLLEVENTS,-1);
+        handle_events(epollfd,events,ret,listenfd,buf);
+    }
+    close(epollfd);
+}
+
+static void handle_events(int epollfd,struct epoll_event *events,int num,int listenfd,char *buf)
+{
+    int i;
+    int fd;
+    //进行选好遍历
+    for (i = 0;i < num;i++)
+    {
+        fd = events[i].data.fd;
+        //根据描述符的类型和事件类型进行处理
+        if ((fd == listenfd) &&(events[i].events & EPOLLIN))
+            handle_accpet(epollfd,listenfd);
+        else if (events[i].events & EPOLLIN)
+            do_read(epollfd,fd,buf);
+        else if (events[i].events & EPOLLOUT)
+            do_write(epollfd,fd,buf);
+    }
+}
+static void handle_accpet(int epollfd,int listenfd)
+{
+    int clifd;
+    struct sockaddr_in cliaddr;
+    socklen_t  cliaddrlen = sizeof(cliaddr);
+    clifd = accept(listenfd,(struct sockaddr*)&cliaddr,&cliaddrlen);
+    if (clifd == -1)
+        perror("accpet error:");
+    else
+    {
+        printf("accept a new client: %s:%d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
+        //添加一个客户描述符和事件
+        add_event(epollfd,clifd,EPOLLIN);
     }
 }
 
-void et(struct epoll_event *event, int num, int epollfd, int listenfd)
+static void do_read(int epollfd,int fd,char *buf)
 {
-    char buf[BUFFER_SIZE];
-    for(int i = 0; i < num; i++){
-            int sockfd = event[i].data.fd;
-            if(sockfd == listenfd){
-                    struct sockaddr_in clientaddr;
-                    socklen_t clilen = sizeof(clientaddr);
-                    int connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clilen);
-                    addfd(epollfd, connfd, true);//多connfd开启ET模式
-            }else if(event[i].events & EPOLLIN){
-                    printf("event trigger once\n");
-                    while(1){//这段代码不会重复触发，所以要循环读取数据
-                            memset(buf, '\0', BUFFER_SIZE);
-                            int ret = recv(sockfd, buf, BUFFER_SIZE-1, 0);
-                            if(ret < 0){
-                                    if((errno == EAGAIN) || (errno == EWOULDBLOCK)){
-                                            printf("read later\n");
-                                            break;
-                                    }
-                                    close(sockfd);
-                                    break;
-                            }else if(ret == 0){
-                                    close(sockfd);
-                            }else{
-                                    printf("get %d bytes of content:%s\n", ret, buf);
-                            }
-                    }
-            }else{
-
-                    printf("something else happened \n");
-            }
+    int nread;
+    nread = read(fd,buf,MAXSIZE);
+    if (nread == -1)
+    {
+        perror("read error:");
+        close(fd);
+        delete_event(epollfd,fd,EPOLLIN);
+    }
+    else if (nread == 0)
+    {
+        fprintf(stderr,"client close.\n");
+        close(fd);
+        delete_event(epollfd,fd,EPOLLIN);
+    }
+    else
+    {
+        printf("read message is : %s",buf);
+        //修改描述符对应的事件，由读改为写
+        modify_event(epollfd,fd,EPOLLOUT);
     }
 }
 
-int start_ser(char *ipaddr, char *port)
+static void do_write(int epollfd,int fd,char *buf)
 {
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
-
-        struct sockaddr_in serveraddr;
-        bzero(&serveraddr, sizeof(serveraddr));
-        serveraddr.sin_family = AF_INET;
-        serveraddr.sin_port = htons(atoi(port));
-        inet_pton(AF_INET, ipaddr, &serveraddr.sin_addr);
-
-        bind(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-
-        listen(sock, 128);
-
-        return sock;
+    int nwrite;
+    nwrite = write(fd,buf,strlen(buf));
+    if (nwrite == -1)
+    {
+        perror("write error:");
+        close(fd);
+        delete_event(epollfd,fd,EPOLLOUT);
+    }
+    else
+        modify_event(epollfd,fd,EPOLLIN);
+    memset(buf,0,MAXSIZE);
 }
 
-int main(int argc, char *argv[])
+static void add_event(int epollfd,int fd,int state)
 {
-        int listenfd = start_ser(argv[1], argv[2]);
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&ev);
+}
 
-        struct epoll_event events[MAX_EVENT_NUM];
-        int epollfd = epoll_create(5);
-        if(epollfd < 0){
-                printf("epoll_create err");
-        }
-        addfd(epollfd, listenfd, true);
-        while(1){
-                int ret = epoll_wait(epollfd, events, MAX_EVENT_NUM, -1);
-                if(ret < 0){
-                        printf("epoll failure\n");
-                        break;
-                }
+static void delete_event(int epollfd,int fd,int state)
+{
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_DEL,fd,&ev);
+}
 
-                //lt(events, ret, epollfd, listenfd);//lt模式
-                et(events, ret, epollfd, listenfd);//et模式
-        }
-        close(listenfd);
-        return 0;
+static void modify_event(int epollfd,int fd,int state)
+{
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_MOD,fd,&ev);
 }
