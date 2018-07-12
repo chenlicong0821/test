2586d1dd2940882e2481cac18204bf17aced601c
chenlicong <chenlicong@123.com>
2018-07-12 17:33:34 +0800

    about python datetime and timezone

--- test.py
+++ test.py
@@ -1,11 +1,26 @@
-import cjson
-import json
-import decimal
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
 
-a = decimal.Decimal('5.5')
+import datetime
+import pytz
 
-minDataStrDict = {'date':'20180709','data':[],'prec':a}
-minDataJsonStr = json.dumps(minDataStrDict)
+print repr(pytz.timezone('Asia/Shanghai'))
+print repr(pytz.timezone('Asia/Chongqing'))
 
-print minDataStrDict
-print minDataJsonStr
+tz = pytz.timezone('Asia/Shanghai')
+
+dtNow = datetime.datetime.now(tz)
+print repr(dtNow)
+dtThis = datetime.datetime(2018, 7, 12, 17, 0, 0, tzinfo=tz)
+print repr(dtThis)
+print (dtNow-dtThis).total_seconds()
+
+dtNow2 = datetime.datetime.now()
+print repr(dtNow2)
+dtNow2 = tz.localize(dtNow2)
+print repr(dtNow2)
+dtThis2 = datetime.datetime(2018, 7, 12, 17, 0, 0)
+print repr(dtThis2)
+dtThis2 = tz.localize(dtThis2)
+print repr(dtThis2)
+print (dtNow2-dtThis2).total_seconds()
