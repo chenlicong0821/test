015f1ad76242eac3ebb8b1e1f97e90981f7c33b4
chenlicong <chenlicong@123.com>
2018-07-27 10:58:57 +0800

    python test

--- test.py
+++ test.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-import datetime
-import pytz
+def prettyFloats(obj, precision=4):
+    if isinstance(obj, float):
+        return round(obj, precision)
+    elif isinstance(obj, dict):
+        return dict((k, prettyFloats(v, precision)) for k, v in obj.iteritems())
+    elif isinstance(obj, list):
+        # return map(prettyFloats, obj, [precision]*len(obj))
+        return [prettyFloats(item, precision) for item in obj]
+    elif isinstance(obj, tuple):
+        # return tuple(map(prettyFloats, obj, [precision]*len(obj)))
+        return tuple([prettyFloats(item, precision) for item in obj])
+    return obj
 
-print repr(pytz.timezone('Asia/Shanghai'))
-print repr(pytz.timezone('Asia/Chongqing'))
-
-tz = pytz.timezone('Asia/Shanghai')
-
-dtNow = datetime.datetime.now(tz)
-print repr(dtNow)
-dtThis = datetime.datetime(2018, 7, 12, 17, 0, 0, tzinfo=tz)
-print repr(dtThis)
-print (dtNow-dtThis).total_seconds()
-
-dtNow2 = datetime.datetime.now()
-print repr(dtNow2)
-dtNow2 = tz.localize(dtNow2)
-print repr(dtNow2)
-dtThis2 = datetime.datetime(2018, 7, 12, 17, 0, 0)
-print repr(dtThis2)
-dtThis2 = tz.localize(dtThis2)
-print repr(dtThis2)
-print (dtNow2-dtThis2).total_seconds()
+if __name__ == '__main__':
+    a = [0.33333333333333333333, 0.9999999999999, 0.1]
+    b = { 'a': 0.333333333, 'b': 0.445444444, 'c': 0.293 }
+    c = ({'a': 0.333333333, 'b': 0.445444444, 'c': 0.293}, {'a': 0.333333333, 'b': 0.445444444, 'c': 0.1})
+    a2 = prettyFloats(a,5)
+    b2 = prettyFloats(b)
+    c2 = prettyFloats(c,2)
+    print a2
+    print b2
+    print c2
+    import json
+    print json.dumps(a2)
+    print json.dumps(b2)
+    print json.dumps(c2)
