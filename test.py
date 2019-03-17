fb9d9889961821f43d82a84aea93b1022e6ba7aa
chenlicong <chenlicong@123.com>
2019-03-18 00:05:00 +0800

    test something

--- test.py
+++ test.py
@@ -1,30 +1,46 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-def prettyFloats(obj, precision=4):
-    if isinstance(obj, float):
-        return round(obj, precision)
-    elif isinstance(obj, dict):
-        return dict((k, prettyFloats(v, precision)) for k, v in obj.iteritems())
-    elif isinstance(obj, list):
-        # return map(prettyFloats, obj, [precision]*len(obj))
-        return [prettyFloats(item, precision) for item in obj]
-    elif isinstance(obj, tuple):
-        # return tuple(map(prettyFloats, obj, [precision]*len(obj)))
-        return tuple([prettyFloats(item, precision) for item in obj])
-    return obj
+def triangles():
+    n, l = 0, [1]
+    while n < 10:
+        yield l
+        l = [0]+l
+        llen = len(l)
+        l = [l[i] + l[i+1] if i+1 < llen else l[i] for i in range(llen)]  
+        n = n + 1
+    return 'done'
 
-if __name__ == '__main__':
-    a = [0.33333333333333333333, 0.9999999999999, 0.1]
-    b = { 'a': 0.333333333, 'b': 0.445444444, 'c': 0.293 }
-    c = ({'a': 0.333333333, 'b': 0.445444444, 'c': 0.293}, {'a': 0.333333333, 'b': 0.445444444, 'c': 0.1})
-    a2 = prettyFloats(a,5)
-    b2 = prettyFloats(b)
-    c2 = prettyFloats(c,2)
-    print a2
-    print b2
-    print c2
-    import json
-    print json.dumps(a2)
-    print json.dumps(b2)
-    print json.dumps(c2)
+# 期待输出:
+# [1]
+# [1, 1]
+# [1, 2, 1]
+# [1, 3, 3, 1]
+# [1, 4, 6, 4, 1]
+# [1, 5, 10, 10, 5, 1]
+# [1, 6, 15, 20, 15, 6, 1]
+# [1, 7, 21, 35, 35, 21, 7, 1]
+# [1, 8, 28, 56, 70, 56, 28, 8, 1]
+# [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+n = 0
+results = []
+for t in triangles():
+    print(t)
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+if results == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+    [1, 7, 21, 35, 35, 21, 7, 1],
+    [1, 8, 28, 56, 70, 56, 28, 8, 1],
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+]:
+    print('测试通过!')
+else:
+    print('测试失败!')
