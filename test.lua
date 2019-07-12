3b867905be438de22f4da496b711699e7bba6d52
chenlicong <chenlicong@123.com>
2019-07-12 16:59:18 +0800

    lua test

--- test.lua
+++ test.lua
@@ -21,3 +21,34 @@ print("hello")
 for k,v in pairs(test1) do
 print(k,v)
 end
+
+test2={code = 200, msg = 'no data', data = {}}
+for k,v in pairs(test2) do
+print(k,v)
+end
+
+local a1 = 0
+local a2 = nil
+local a3 = {}
+local a4=''
+
+if a1 then
+    print('a')
+else
+    print('not a')
+end
+if a2 then
+    print('a')
+else
+    print('not a')
+end
+if a3 then
+    print('a')
+else
+    print('not a')
+end
+if a4 then
+    print('a')
+else
+    print('not a')
+end
