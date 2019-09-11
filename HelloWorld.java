4946b773ad3a4ff140ab50835a133b58bcf2beac
chenlicong <chenlicong@123.com>
2019-09-11 16:08:08 +0800

    mod

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		int count =0;  
-		for(int i=0;i<10;i++){  
-		 count=count++;  
-		}  
-		System.out.println("count="+count);  
+		int count =0;
+		for(int i=0;i<10;i++){
+		 count=count++;
+		}
+		System.out.println("count="+count);
 	}
 }
